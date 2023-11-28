268a11a94dd79e87c33251ace3883815bf7966c4
Auberginie <mona.lorenz@icloud.com>
2023-11-28 20:35:49 +0100

    version 1.1

---
+++ _header.php
@@ -0,0 +1,53 @@
+<header id="header">
+    <div class="container">
+
+        <!-- LOGO -->
+        <a class="logo" href="index.php"><img src="img/logo.png" width="150px" height="150px"></a>
+
+        <!-- MENU -->
+        <div class="menu">
+
+            <!-- BUTTON -->
+            <a class="btn" href="meldung_start.php" titel="Meldung machen!" alt="Meldung machen!">Meldung machen!</a>
+
+            <!-- HAMBURGER  MENU -->
+            <div class="navigation">
+                <input class="burger_checkbox" id="burger" type="checkbox"/>
+                <label class="burger_icon" for="burger">
+                <span class="inner">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </span>
+                </label>
+                <div class="menu_overlay">
+                    <nav class="nav">
+                        <ul class="nav-main">
+                            <li>
+                                <a href="index.php" title="Home">Home</a>
+                            </li>
+                            <li>
+                                <a href="meldung_start.php" title="Meldung!">Meldung!</a>
+                            </li>
+                            <li>
+                                <a href="ueberuns.php" title="Über Uns">Über Uns</a>
+                            </li>
+                            <li>
+                                <a href="kontakt.php" title="Kontakt">Kontakt</a>
+                            </li>
+                        </ul>
+                        <hr>
+                        <ul class="nav-service">
+                            <li>
+                                <a href="impressum.php" title="Impressum">Impressum</a>
+                            </li>
+                            <li>
+                                <a href="datenschutz.php" title="Datenschutz">Datenschutz</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ ueberuns.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Über Uns - Lehrmittel-Meldestelle</title>
+
+    <!-- METADATEN -->
+    <meta name="robots" content="index,follow">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+    <!-- STYLE -->
+    <link rel="stylesheet" href="font/_fonts.css">
+    <link rel="stylesheet" href="css/app.css">
+
+    <!-- ICON LIBARY für MAIL ICON -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<body>
+
+    <!-- INCLUDE HEADER -->
+    <?php include '_header.php' ?>
+
+    <main  class="bg_red">
+        <div class="inside">
+            <div class="container">
+                <p class="label">Diversität & Repräsentation.</p>
+                <h1>Über uns</h1>
+                <p class="teaser">Ein Studiumsprojekt, welches Sinn ergeben und genutzt werden soll.</p>
+                <p>In der Kinder- und Jugend-Literatur ist glücklicherweise ein Wandel, hin zu mehr Diversität, zu beobachten. In den Schulen sind wir davon leider noch weit entfernt - und genau da wollen wir anknüpfen. In einer Datenbank sammeln wir alle eure Meldungen in anonymisierter Form und wollen diese dann den Lehrmittelverbänden gebündelt zukommen lassen um ihnen aufzuzeigen wieviel Verbesserungspotential in all diesen Lehrmitteln steckt. Diese Plattform soll von Lehrpersonen genauso wie von Eltern und Erziehungsberechtigten genutzt werden.</p>
+            </div>
+        </div>
+    </main>
+
+    <!-- JAVASCRIP -->
+    <script src="js/script.js"></script>
+
+</body>
+</html>
