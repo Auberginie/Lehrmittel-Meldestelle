a202beac034c4481db65318a58330ee0c81c98c8
Auberginie <mona.lorenz@icloud.com>
2023-12-05 19:50:31 +0100

    Glossar-Akkordeon

--- glossar.php
+++ glossar.php
@@ -28,32 +28,125 @@
                 <p class="label">Diversität & Repräsentation.</p>
                 <h1>Glossar</h1>
                 <p class="teaser">Hier erklären wir alle Begriffe, welche wir bei der Meldungs-Eingabe verwenden.</p>
+                
+                <div class="accordion">
+	                
+								  <div class="accordion-item" id="rassismus">
+								    <h3 class="accordion-button">Rassismus <span class="toggle"></span></h3>
+								    <div class="accordion-content" style="max-height: 0px;">
+								      <p>Die Eidgenössische Kommission gegen Rassismus EKR definiert Rassismus wie folgt:</p>
+								      <ul>
+								        <li>Konstruktion und Betonung tatsächlicher oder fiktiver Unterschiede zwischen der Person, die Rassismus ausübt und des Opfers</li>
+								        <li>Wertung dieser Unterschiede zum Nutzen der Person, die Rassismus ausübt und zum Schaden des Opfers</li>
+								        <li>Verallgemeinerung und Verabsolutierung dieser Unterschiede</li>
+								        <li>Legitimierung einer Aggression oder eines Privilegs</li>
+								      </ul>
+								      <p>Nebst dieser engen Definition, die insbesondere den klassischen Rassismus in seiner biologistischen Prägung beschreibt, gibt es den Rassismusbegriff «im weiteren Sinne», der sich auf kulturelle, psychologische, soziale oder metaphysische Argumente abstützt...</p>
+								    </div>
+								  </div>
+								  
+								  <div class="accordion-item" id="vielfalt">
+								    <h3 class="accordion-button">Fehlende ethnische & kulturelle Vielfalt<span class="toggle"></span></h3>
+								    <div class="accordion-content" style="max-height: 0px;">
+								      <p>Der Oxford Dictionary beschreibt ethnische & kulturelle Vielfalt als „die Existenz verschiedener kultureller oder ethnischer Gruppen innerhalb einer Gesellschaft“...</p>
+								    </div>
+								  </div>
+								
+								  <div class="accordion-item" id="ableismus">
+								    <h3 class="accordion-button">Ableismus<span class="toggle"></span></h3>
+								    <div class="accordion-content" style="max-height: 0px;">
+								      <p>Menschen mit Behinderung treffen im Alltag häufig auf Ableismus...</p>
+								      <p>...</p>
+								    </div>
+								  </div>
+								
+								  <div class="accordion-item" id="able-bodied">
+								    <h3 class="accordion-button">Able-bodied<span class="toggle"></span></h3>
+								    <div class="accordion-content" style="max-height: 0px;">
+								      <p>Lorem Ipsum</p>
+								    </div>
+								  </div>
+								
+								  <div class="accordion-item" id="sexismus">
+								    <h3 class="accordion-button">Sexismus<span class="toggle"></span></h3>
+								    <div class="accordion-content" style="max-height: 0px;">
+								      <p>Die Brava-Organisation erklärt den Begriff Sexismus mit folgenden Worten...</p>
+								      <p>...</p>
+								    </div>
+								  </div>
+								
+								  <div class="accordion-item" id="queerfeindlichkeit">
+								    <h3 class="accordion-button">Queerfeindlichkeit<span class="toggle"></span></h3>
+								    <div class="accordion-content" style="max-height: 0px;">
+								      <p>Das Queer-Lexikon beschreibt Queerfeindlichkeit folgendermassen...</p>
+								      <p>...</p>
+								    </div>
+								  </div>
+								
+								  <div class="accordion-item" id="stereotype">
+								    <h3 class="accordion-button">Bedienen von Stereotypen<span class="toggle"></span></h3>
+								    <div class="accordion-content" style="max-height: 0px;">
+								      <p>Lorem Ipsum</p>
+								    </div>
+								  </div>
+								
+								  <div class="accordion-item" id="heteronormativitaet">
+								    <h3 class="accordion-button">Heteronormativität<span class="toggle"></span></h3>
+								    <div class="accordion-content" style="max-height: 0px;">
+								      <p>Behauptung von Zweigeschlechtlichkeit als Naturgesetz und Naturalisierung von einem gegengeschlechtlichen sexuellen Begehren...</p>
+								      <p>...</p>
+								    </div>
+								  </div>
+								
+								
+								</div>
+                
                 <h3>Rassismus</h3>
                 <p>Die Eidgenössische Kommission gegen Rassismus EKR definiert Rassismus wie folgt: 
+                    Vier Elemente sind für rassistische Ideologien charakteristisch:
                     <ul>
-                    <li>Vier Elemente sind für rassistische Ideologien charakteristisch:</li>
-                    <ul>Konstruktion und Betonung tatsächlicher oder fiktiver Unterschiede zwischen dem Rassisten und seinem Opfer</ul>
-                    <ul>Wertung dieser Unterschiede zum Nutzen des Rassisten und zum Schaden des Opfers</ul>
-                    <ul>Verallgemeinerung und Verabsolutierung dieser Unterschiede</ul>
-                    <ul>Legitimierung einer Aggression oder eines Privilegs</ul>
-                Nebst dieser engen Definition, die insbesondere den klassischen Rassismus in seiner biologistischen Prägung beschreibt, gibt es den Rassismusbegriff «im weiteren Sinne», der sich auf kulturelle, psychologische, soziale oder metaphysische Argumente abstützt. Vom Rassismus ist ferner die Rassendiskriminierung zu unterscheiden. Im Gegensatz zu Rassismus, der eine Ideologie oder geistige Haltung bezeichnet, versteht man unter Rassendiskriminierung eine konkrete Handlung, die eine als minderwertig bezeichnete Gruppe oder Einzelperson im realen Leben benachteiligt. Ersteres hat meistens verbale Diffamierung zur Folge, und diese dann konkrete Diskriminierung.</p>
-                Grundsätzlich ist zu beobachten, dass die meisten Fälle rassistischer Diskriminierung in der Schweiz nicht ideologisch begründet sind. Sie basieren vielmehr auf Unwissen, diffusen Ängsten, Aggressionen, Vorurteilen und mangelndem Einfühlungsvermögen. Aber unabhängig davon, ob eine ideologische Bedeutung vorhanden ist oder nicht, geht es beim Einsatz gegen Rassismus zuallererst darum, anzuerkennen, dass es rassistische Diskriminierung gibt, und zwar auf struktureller, institutioneller wie individueller Ebene. Es gilt, die Verletzung, die Opfer derartiger Diskriminierungen erleiden, anzuerkennen.
+                        <li>Konstruktion und Betonung tatsächlicher oder fiktiver Unterschiede zwischen der Person, die Rassismus ausübt und des Opfers</li>
+                        <li>Wertung dieser Unterschiede zum Nutzen der Person, die Rassismus ausübt und zum Schaden des Opfers</li>
+                        <li>Verallgemeinerung und Verabsolutierung dieser Unterschiede</li>
+                        <li>Legitimierung einer Aggression oder eines Privilegs</li>
+                    </ul>
+
+                    Nebst dieser engen Definition, die insbesondere den klassischen Rassismus in seiner biologistischen Prägung beschreibt, gibt es den Rassismusbegriff «im weiteren Sinne», der sich auf kulturelle, psychologische, soziale oder metaphysische Argumente abstützt. Vom Rassismus ist ferner die Rassendiskriminierung zu unterscheiden. Im Gegensatz zu Rassismus, der eine Ideologie oder geistige Haltung bezeichnet, versteht man unter Rassendiskriminierung eine konkrete Handlung, die eine als minderwertig bezeichnete Gruppe oder Einzelperson im realen Leben benachteiligt. Ersteres hat meistens verbale Diffamierung zur Folge, und diese dann konkrete Diskriminierung.</p>
+                    Grundsätzlich ist zu beobachten, dass die meisten Fälle rassistischer Diskriminierung in der Schweiz nicht ideologisch begründet sind. Sie basieren vielmehr auf Unwissen, diffusen Ängsten, Aggressionen, Vorurteilen und mangelndem Einfühlungsvermögen. Aber unabhängig davon, ob eine ideologische Bedeutung vorhanden ist oder nicht, geht es beim Einsatz gegen Rassismus zuallererst darum, anzuerkennen, dass es rassistische Diskriminierung gibt, und zwar auf struktureller, institutioneller wie individueller Ebene. Es gilt, die Verletzung, die Opfer derartiger Diskriminierungen erleiden, anzuerkennen.
+                
                 <h3>fehlende ethnische & kulturelle Vielfalt</h3>
-                <p>Lorem Ipsum</p>
+                    <p>Der Oxford Dictionary beschreibt ethnische & kulturelle Vielfalt als „die Existenz verschiedener kultureller oder ethnischer Gruppen innerhalb einer Gesellschaft“. Mit anderen Worten: Es ist eine Population, in der alle Unterschiede vertreten sind.</p>
+                
                 <h3>Ableismus</h3>
-                <p>Lorem Ipsum</p>
+                    <p> Menschen mit Behinderung treffen im Alltag häufig auf Ableismus. Andere Leute begegnen ihnen dann mit bestimmten Vorannahmen, ohne die Personen direkt zu fragen. Zum Beispiel danach, was sie können oder nicht können. Wir erklären euch die Definition von Ableismus und wie du damit umgehen kannst.</p>
+                    <p> Werden Menschen im Alltag auf ihre körperliche oder psychische Behinderung reduziert, spricht man in der Fachsprache von Ableismus. Genauer bedeutet Ableismus also, dass Menschen mit Behinderung von anderen Menschen ohne Behinderung auf die Merkmale reduziert werden, in denen sie sich vom vermeintlichen Normalzustand unterscheiden. Dies können zum Beispiel sichtbare oder unsichtbare Merkmale sein, also ein Rollstuhl oder eine geistige Behinderung. Von diesen Merkmalen wird anschließend, ohne vorher nachzufragen, beispielsweise darauf geschlossen, was die Person mit der Behinderung kann oder nicht kann oder wie sich die Person fühlt. Das ist eine Art von Diskriminierung. Diese Definition wurde von Aktion Mensch verfasst.</p>
+            
                 <h3>Able-bodied</h3>
-                <p>Lorem Ipsum</p>
+                    <p>Lorem Ipsum</p>
+                
                 <h3>Sexismus</h3>
-                <p>Lorem Ipsum</p>
+                    <p>Die Brava-Organisation erklärt den Begriff Sexismus mit folgenden Worten:  Sexismus umfasst die Zuschreibung von Eigenschaften basierend auf Geschlechtervorstellungen sowie die Diskriminierung von Menschen aufgrund von Geschlecht. Dadurch werden Menschen in ihrer freien Entfaltung gehindert. Sexismus kann alle Menschen betreffen, aber auch heute noch sind Frauen weltweit – und auch in der Schweiz – stärker betroffen.</p>
+                    <p>Sexismus dient als Grundlage eines patriarchalen Systems, welches Menschen in zwei Geschlechter einteilt und hierarchisch ordnet. In diesem System wird Frauen eine untergeordnete Position zugeschrieben und Männern eine privilegierte, machtvolle Position. Sexismus durchdringt jeden Bereich der Gesellschaft und zeigt sich sowohl in alltäglichen Handlungen in Form sexistischer Witze oder Herabwürdigungen wie auch strukturell. Er ist tief in unserer Gesellschaft verankert, etwa in Arbeitswelt, Politik und Medien (sexistische Stereotype in der Werbung, geringe Chancen auf dem Arbeitsmarkt, Lohnungleichheit, ungleiche Verteilung der unbezahlten Arbeit, Untervertretung von Frauen in der Politik und in Entscheidungspositionen).</p>
+                    <p>In weiten Teilen der Gesellschaft hält sich hartnäckig die Vorstellung, es gebe nur zwei Geschlechter und diese seien von Natur aus unterschiedlich. Dabei wird Männlichkeit auf- und Weiblichkeit abgewertet, was die Diskriminierung von Frauen legitimiert.</p>
+                    <p>Wir kritisieren Vorstellungen und Bilder davon, wie sich ein Mensch oder eine Gruppe von Menschen aufgrund von Geschlecht, Geschlechtsidentität oder sexueller Orientierung zu verhalten hat. Erlebte Diskriminierung ist keine Erfahrung von Einzelpersonen, sondern hat System und trifft alle, die die gesellschaftlichen Vorstellungen nicht erfüllen.</p>
+                    <p>Sexismus steht mit anderen Formen von Diskriminierung in Wechselwirkung. So machen Women of Color andere Sexismuserfahrungen als weisse Frauen, Frauen mit Behinderung andere als Frauen ohne Behinderung. </p>
+                
                 <h3>Queerfeindlichkeit</h3>
-                <p>Lorem Ipsum</p>
+                    <p>Das Queer-Lexikon beschreibt Queerfeindlichkeit folgendermassen:  
+                    <p>Queerfeindlichkeit ist die Diskriminierung von queeren Menschen. Sie zeigt sich z.B. durch Ablehnung, Wut, Intoleranz, Vorurteile, Unbehagen oder körperliche bzw. psychische Gewalt. Queerfeindlichkeit kann aber auch nicht queere Menschen treffen, die als queer wahrgenommen werden oder die (scheinbar) von den gesellschaftlichen Regeln und Normen zu Sexualität und Geschlecht abweichen.</p>
+
+Internalisierte Queerfeindlichkeit beschreibt, dass Queerfeindlichkeit gegen die eigene Queerness und damit gegen sich selbst gerichtet ist. Dies passiert oft in einer queerfeindlichen Umgebung und/oder vor dem eigenen inneren Coming out.</p>
+                
                 <h3>Bedienen von Stereotypen</h3>
-                <p>Lorem Ipsum</p>
+                    <p>Lorem Ipsum</p>
+                
                 <h3>Heteronormativität</h3>
-                <p>Lorem Ipsum</p>
+                    <p>Behauptung von Zweigeschlechtlichkeit als Naturgesetz und Naturalisierung von einem gegengeschlechtlichen sexuellen Begehren. Zahlreiche Individuen lehnen es allerdings ab, verbal, sozial oder physisch geschlechtlich vereindeutigt zu werden. Ausserdem wird jede zweitausendste Person intersexuell geboren. Hinzuzufügen ist, dass in vielen Gesellschaften ein differenzierteres Konzept von geschlechtlicher Zugehörigkeit existiert.
+                    <p>Konsequenzen für die Forschungsethik</p>
+                    <p>«Geschlechtstypisches» Verhalten und ein statisches Verständnis von Zweigeschlechtlichkeit sind nicht naturgegeben, sondern werden permanent reproduziert («doing gender») – unter anderem dadurch, dass Erziehungsberechtigte, Schüler_innen, Lehrpersonen oder Forscher_innen heteronormative Zuordnungen vornehmen. Daher: Forschung am IAE verzichtet auf die reflexhafte Einteilung in «Mädchen» und «Jungen». Geschlecht wird als Analysekategorie verwendet, nicht als naturgegebene Eigenschaft. Das heisst, es wird gefragt, auf welche Weise sich geschlechtliche Zuschreibungen (nicht: «das Geschlecht») jeweils auf Lehren und Lernen auswirken. Geschlecht wird ausserdem intersektionell betrachtet, das heisst im Zusammenwirken mit anderen Kategorien wie soziale Herkunft oder Bildungshintergrund.</p>
+                
                 <h3>binäre Geschlechterrollen</h3>
-                <p>Lorem Ipsum</p>
+                    <p>Lorem Ipsum</p>
             </div>
         </div>
     </main>
